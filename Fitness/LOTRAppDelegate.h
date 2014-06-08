52fbe74565109354acc9b395bf7ec43ddcf71367
Larry Xiao <xiaodi@sjtu.edu.cn>
2014-06-08 23:10:10 +0800

    Initial Commit

---
+++ Fitness/LOTRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LOTRAppDelegate.h
+//  Fitness
+//
+//  Created by Larry Xiao on 6/8/14.
+//  Copyright (c) 2014 LOTR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOTRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fitness/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fitness
+//
+//  Created by Larry Xiao on 6/8/14.
+//  Copyright (c) 2014 LOTR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LOTRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LOTRAppDelegate class]));
+    }
+}
